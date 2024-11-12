477704cc82cc0efd6f95a1b49c90284d21423e31
yeliannys16 <161481503+yeliannys16@users.noreply.github.com>
2024-11-12 09:33:09 -0300

    Add files via upload

---
+++ estudo.2d-main/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'arvore de natal',
+    'o que se coloca no topo da arvore de natal?',
+    'uma estrela'
+)
+
+criaCartao(
+    'mes',
+    'em que mes é celebrado o natal?',
+    'dezembro'
+)
+
+criaCartao(
+    'cor',
+    'qual cor é associado ao natal?',
+    'vermelho e verde'
+)
+
+criaCartao(
+    'natal',
+    'qual é o nome do treno do papai noel?',
+    'treno magico'
+)
